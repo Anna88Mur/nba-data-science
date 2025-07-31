506593e5f846a0538040d387329d4942d8792408
Isabelle Haehl <isabelle.haehl@mail.de>
2025-07-31 10:55:05 +0200

    Update 2_👥_Unser_Team.py
    Letzte Veränderung wegen der Qualität wieder rückgängig gemacht

--- pages/2_👥_Unser_Team.py
+++ pages/2_👥_Unser_Team.py
@@ -36,7 +36,7 @@ st.markdown("""
 
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("images/Isabelle_1.jpg", width=200)
+    st.image("images/Isabelle.png", width=200)
 with col2:
     st.markdown(f"""
     <strong>Isabelle Haehl</strong>
